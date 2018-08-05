2f51db15e25565fe7e80090bcf56f4b78fc5908a
Domenico Giambra <zorinik@gmail.com>
2018-08-05 15:35:56 +0200

    Added Event polyfill

--- files/polyfills.js
+++ files/polyfills.js
@@ -1,3 +1,4 @@
+// Array.from
 if (!Array.from) {
 	Array.from = (function () {
 		var toStr = Object.prototype.toString;
@@ -78,3 +79,19 @@ if (!Array.from) {
 		};
 	}());
 }
+
+// Event
+(function () {
+	if (typeof window.CustomEvent === "function") return false; //If not IE
+
+	function CustomEvent(event, params) {
+		params = params || {bubbles: false, cancelable: false, detail: undefined};
+		var evt = document.createEvent('CustomEvent');
+		evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+		return evt;
+	}
+
+	CustomEvent.prototype = window.Event.prototype;
+
+	window.Event = CustomEvent;
+})();
