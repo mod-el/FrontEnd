f8a3862ede37964c0eb51a1d311b0f601219c204
Domenico Giambra <zorinik@gmail.com>
2018-11-25 23:10:29 +0100

    New "entities" function

--- files/zklibs.js
+++ files/zklibs.js
@@ -543,3 +543,9 @@ function handleAjaxResponse(text) {
 
 	return r;
 }
+
+function entities(str) {
+	return str.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+		return '&#' + i.charCodeAt(0) + ';';
+	});
+}
