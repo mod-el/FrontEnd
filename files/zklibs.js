82801f4e3dbc9bf628347ff19a9e1b3d1887c03f
Domenico Giambra <zorinik@gmail.com>
2019-02-09 22:39:06 +0100

    You can now specify a path when deleting a cookie

--- files/zklibs.js
+++ files/zklibs.js
@@ -413,14 +413,14 @@ function getCookie(name) {
 	var ca = document.cookie.split(';');
 	for (var i = 0; i < ca.length; i++) {
 		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
 	}
 	return null;
 }
 
-function deleteCookie(name) {
-	setCookie(name, "", -1);
+function deleteCookie(name, path) {
+	setCookie(name, '', -1, path);
 }
 
 var changedHtmlCallbacks = [];
